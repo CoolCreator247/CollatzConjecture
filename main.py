8f5d13cf335abb742f5a02a6eba2ce95687129fe
Faris Allafi <96661970+CoolCreator247@users.noreply.github.com>
2023-02-09 02:51:50 +0000

    fix

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import time
 
-def collatz(num, level):
-    tree = ""
+def main():
+    num = float(input(">"))
+    print(num)
     while num != 1:
         if (num % 2) == 0:
             num = num / 2
-            tree += " " * level + "|\n"
-            tree += " " * level + "v\n"
-            tree += " " * level + str(num) + "\n"
         else:
             num = num * 3 + 1
-            tree += " " * level + "|\n"
-            tree += " " * level + "v\n"
-            tree += " " * level + str(num) + "\n"
-        level += 2
-        tree += collatz(num, level)
-    return tree
-
-tree = collatz(100, 0)
-print(tree)
+        print(num)
+        #time.sleep(.15)
+main()
